34451b3a3d4ae858df8c2cfb4c2703d75783571f
Capi Etheriel <barraponto@gmail.com>
2018-03-16 00:34:56 -0300

    primeiro rascunho de home

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-jumbotron tag="section" class="home"
+    header-level="4" fluid
+    header="Conta Alta?">
+    <template slot="lead">
+      Monitore o consumo de água do seu condomínio e evite surpresas no fim do mês!
+    </template>
+    <b-button variant="primary">Cadastre-se</b-button>
+    <b-button variant="secondary">Saiba Mais</b-button>
+  </b-jumbotron>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
