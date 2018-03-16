ec142f28bf77305a54a759af9b07f150dff4a204
Capi Etheriel <barraponto@gmail.com>
2018-03-15 23:32:37 -0300

    iniciando o template da home

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="app">
+    <Header />
+    <main id="app">
+      <router-view/>
+    </main>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header';
+
 export default {
   name: 'App',
+  components: {
+    Header,
+  },
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <b-navbar variant="dark" type="dark">
+        <b-navbar-brand href="#">Condágua</b-navbar-brand>
+    </b-navbar>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style></style>
