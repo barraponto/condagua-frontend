e38900458492e426d8d18d904803395fdaa68b5d
Capi Etheriel <barraponto@gmail.com>
2018-03-16 14:34:31 -0300

    wip da tela de dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,8 +1,17 @@
 <template>
   <b-jumbotron tag="section" class="dashboard" bg-variant="transparent">
     <template slot="lead">
-      Bem vindo, {{ $store.state.user.first }}
+      Bem vindo, {{ $store.state.user.name.first }}
     </template>
+    <div v-if="condos.length " class="">
+      <div class="condo" v-for="condo in condos">
+        Relogio
+      </div>
+    </div>
+    <div v-else class="no-condos">
+      <p>Você ainda não cadastrou nenhum condomínio!</p>
+      <b-button variant="primary" size="lg" block>Cadastre Agora</b-button>
+    </div>
   </b-jumbotron>
 </template>
 
@@ -11,6 +20,7 @@ export default {
   name: 'Home',
   data() {
     return {
+      condos: [],
     };
   },
 };
